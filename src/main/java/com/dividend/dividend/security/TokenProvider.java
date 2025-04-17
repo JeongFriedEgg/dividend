c429229adcaf317ceb731a1ab01405613479fd2a
jeongbanseog <wibalislfilask@gmail.com>
2025-04-17 12:24:51 +0900

    feat : 토큰 생성 및 유효성 검증에 대한 기능 구현

---
+++ src/main/java/com/dividend/dividend/security/TokenProvider.java
@@ -0,0 +1,60 @@
+package com.dividend.dividend.security;
+
+import com.dividend.dividend.service.MemberService;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class TokenProvider {
+
+    private static final String KEY_ROLES = "roles";
+    private static final long TOKEN_EXPIRE_TIME = 1000 * 60 * 60; // 1 hour
+
+    private final MemberService memberService;
+
+    @Value("{spring.jwt.secret}")
+    private String secretKey;
+
+    public String generateToken(String username, List<String> roles) {
+        Claims claims = (Claims) Jwts.claims().setSubject(username);
+        claims.put(KEY_ROLES,roles);
+
+        var now = new Date();
+        var expiredDate = new Date(now.getTime() + TOKEN_EXPIRE_TIME);
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(expiredDate)
+                .signWith(SignatureAlgorithm.HS512, secretKey)
+                .compact();
+    }
+
+    public boolean validateToken(String token) {
+        if (!StringUtils.hasText(token)) return false;
+
+        var claims = parseClaims(token);
+        return !claims.getExpiration().before(new Date());
+    }
+
+    private Claims parseClaims(String token) {
+        try {
+            JwtParser parser = Jwts.parser()
+                    .setSigningKey(Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8)))
+                    .build();
+
+            return parser.parseClaimsJws(token).getBody();
+        }catch (ExpiredJwtException e){
+            return e.getClaims();
+        }
+    }
+}
