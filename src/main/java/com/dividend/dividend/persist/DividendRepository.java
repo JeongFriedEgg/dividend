790e7b582cfd589f4f466ea084eb721cb9a07fb8
jeongbanseog <wibalislfilask@gmail.com>
2025-04-17 11:32:26 +0900

    feat : 배당금 정보 스크랩 스케쥴링 구현
    상세 : 매일 실제의 배당금 데이터가 업데이트 되기 때문에, 자정에 회사 정보들에 대한 배당금 데이터를 다시 스크랩한다. 스케쥴링을 통해 자정에 자동으로 스크랩할 수 있도록 한다. 한꺼번에 요청을 많이 보내면 해당 서버에 부하가 갈 수 있기 때문에, Thread.sleep 으로 약 3초 간격으로 스크랩을 한다.

--- src/main/java/com/dividend/dividend/persist/DividendRepository.java
+++ src/main/java/com/dividend/dividend/persist/DividendRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
@@ -13,4 +14,6 @@ public interface DividendRepository extends JpaRepository<DividendEntity, Long> 
     void deleteAllByCompanyId(Long id);
 
     List<DividendEntity> findAllByCompanyId(Long companyId);
+
+    boolean existsByCompanyIdAndDate(Long companyId, LocalDateTime date);
 }
---
+++ src/main/java/com/dividend/dividend/scheduler/ScraperScheduler.java
@@ -0,0 +1,56 @@
+package com.dividend.dividend.scheduler;
+
+import com.dividend.dividend.model.Company;
+import com.dividend.dividend.model.ScrapedResult;
+import com.dividend.dividend.persist.CompanyRepository;
+import com.dividend.dividend.persist.DividendRepository;
+import com.dividend.dividend.persist.entity.CompanyEntity;
+import com.dividend.dividend.persist.entity.DividendEntity;
+import com.dividend.dividend.scraper.Scraper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class ScraperScheduler {
+
+    private final CompanyRepository companyRepository;
+    private final DividendRepository dividendRepository;
+
+    private final Scraper yahooFinanceScraper;
+
+    @Scheduled(cron = "${scheduler.scrap.yahoo}")
+    public void yahooFinanceScheduling() {
+        log.info("scraping scheduler is started");
+
+        List<CompanyEntity> companies = companyRepository.findAll();
+
+        for (var company : companies) {
+            log.info("scraping scheduler is started -> " + company.getName());
+
+            ScrapedResult scrapedResult = yahooFinanceScraper.scrap(
+                    new Company(company.getName(), company.getTicker()));
+
+            scrapedResult.getDividends().stream()
+                    .map(e -> new DividendEntity(company.getId(), e))
+                    .forEach(e -> {
+                        boolean exists = dividendRepository.existsByCompanyIdAndDate(e.getCompanyId(), e.getDate());
+                        if (!exists) {
+                            dividendRepository.save(e);
+                            log.info("insert new dividend -> " + e.toString());
+                        }
+                    });
+
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
+}
