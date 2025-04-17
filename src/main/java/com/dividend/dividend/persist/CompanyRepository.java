aee4e2ce113342cfa44d7a29307b79957ad31ee2
jeongbanseog <wibalislfilask@gmail.com>
2025-04-17 10:08:30 +0900

    feat : 회사정보, 배당금에 대한 엔티티, 리포지트리 작성

---
+++ src/main/java/com/dividend/dividend/persist/CompanyRepository.java
@@ -0,0 +1,9 @@
+package com.dividend.dividend.persist;
+
+import com.dividend.dividend.persist.entity.CompanyEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CompanyRepository extends JpaRepository<CompanyEntity, Long> {
+}
